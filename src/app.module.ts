1433da258622729ea09a88efa028b99c6b69dbf8
Don Tay Xi Cheng <dontay0209@gmail.com>
2021-05-30 23:38:55 +0800

    Add validation and load config env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: './config/config.env',
+      isGlobal: true, // Env config file set as global import, no need to import into individual modules
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+      }),
+    }),
+  ],
   controllers: [AppController, AuthController],
   providers: [AppService],
 })

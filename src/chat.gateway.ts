25d6af12520e286f4575403a92e7c925cd866af5
Don Tay Xi Cheng <dontay0209@gmail.com>
2021-06-06 23:17:17 +0800

    Add connection hook

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,14 +1,20 @@
 import {
   MessageBody,
+  OnGatewayConnection,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
+import { Server } from 'http';
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection {
   @WebSocketServer()
-  private server;
+  private server: Server;
+
+  handleConnection(): void {
+    console.log('User connected to chat gateway');
+  }
 
   @SubscribeMessage('message')
   handleMessage(@MessageBody() msg: string): void {

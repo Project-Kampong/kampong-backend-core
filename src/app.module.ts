0a9282589ac80537182d2aea1deaa1f3e69d647a
Don Tay Xi Cheng <dontay0209@gmail.com>
2021-06-19 16:35:45 +0800

    Add introspection to graphql playground

--- src/app.module.ts
+++ src/app.module.ts
@@ -30,6 +30,7 @@ import { AuthModule } from './auth/auth.module';
       installSubscriptionHandlers: true,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       playground: true,
+      introspection: true,
     }),
     UsersModule,
     AuthModule,

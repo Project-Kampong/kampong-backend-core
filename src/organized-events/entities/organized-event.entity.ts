faa79e193f66b2425a0a918078c5fb5576ebf1c1
Don Tay Xi Cheng <dontay0209@gmail.com>
2021-06-21 00:29:37 +0800

    Add initial organizedEvent schema

--- src/organized-events/entities/organized-event.entity.ts
+++ src/organized-events/entities/organized-event.entity.ts
@@ -1,7 +1,71 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, ID, Int } from '@nestjs/graphql';
 
 @ObjectType()
+class Question {
+  @Field(() => ID)
+  _id: string;
+  @Field(() => ID)
+  userId: string;
+  @Field()
+  username: string;
+  @Field()
+  questionText: string;
+  @Field(() => Int)
+  voteCount: number;
+  @Field(() => [Answer])
+  answers: Answer[];
+  @Field()
+  createdAt: Date;
+  @Field()
+  updatedAt: Date;
+}
+
+@ObjectType()
+class Answer {
+  @Field(() => ID)
+  _id: string;
+  @Field(() => ID)
+  userId: string;
+  @Field()
+  username: string;
+  @Field()
+  answerText: string;
+  @Field(() => Int)
+  voteCount: number;
+  @Field()
+  createdAt: Date;
+  @Field()
+  updatedAt: Date;
+}
+@ObjectType()
+class QnaSession {
+  @Field(() => [Question])
+  questions: Question[];
+}
+@ObjectType()
 export class OrganizedEvent {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => ID)
+  _id: string;
+  @Field(() => String, { description: 'Example field (placeholder)' })
+  eventName: string;
+  @Field()
+  startDate: Date;
+  @Field()
+  endDate: Date;
+  @Field()
+  description: string;
+  @Field(() => ID, { description: `Organizer's user ID` })
+  organizerId: string;
+  @Field()
+  category: string;
+  @Field()
+  eventPassword: string;
+  @Field()
+  qnaSessionOpen: boolean;
+  @Field(() => QnaSession)
+  qnaSession: QnaSession;
+  @Field()
+  createdAt: Date;
+  @Field()
+  updatedAt: Date;
 }

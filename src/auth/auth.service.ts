3cd876b9865ef26fe885bf4538f9c9c005f4e3e2
Don Tay Xi Cheng <dontay0209@gmail.com>
2021-06-24 12:47:07 +0800

    Remove unused import'

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { sign } from 'jsonwebtoken';
 import * as bcrypt from 'bcryptjs';
 import { isEmpty } from 'lodash';
